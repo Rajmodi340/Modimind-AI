6133a516a168b21c926f950e139baa91104be45e
Raj Modi <modiraj340@gmail.com>
2026-07-05 22:51:09 +0530

    setup-frontend/backend

---
+++ backend/gateway/index.js
@@ -0,0 +1,7 @@
+import express from "express"
+import dotenv from "dotenv"
+const app=express()
+dotenv.config()
+app.listen(process.env.PORT,()=>{
+console.log(`gateway started at ${process.env.PORT}`)
+})
